656dc76ea51f07422ba1bba2d90bd58d7c429b84
velopert <public.velopert@gmail.com>
2016-08-01 00:04:55 +0900

    05. MEMO API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import account from './account';
+import memo from './memo';
 
 const router = express.Router();
 router.use('/account', account);
+router.use('/memo', memo);
 
 export default router;
