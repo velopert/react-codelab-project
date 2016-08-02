3c28af7edd0591435917d02632f14e65fa571d5a
velopert <public.velopert@gmail.com>
2016-08-02 22:31:03 +0900

    19.INF_SCROLL

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,6 +9,10 @@ class Home extends React.Component {
         super(props);
         this.handlePost = this.handlePost.bind(this);
         this.loadNewMemo = this.loadNewMemo.bind(this);
+        this.loadOldMemo = this.loadOldMemo.bind(this);
+        this.state = {
+            loadingState: false
+        };
     }
 
     componentDidMount() {
@@ -20,15 +24,53 @@ class Home extends React.Component {
             );
         };
 
+        const loadUntilScrollable = () => {
+            // IF THE SCROLLBAR DOES NOT EXIST,
+            if($("body").height() < $(window).height()) {
+                this.loadOldMemo().then(
+                    () => {
+                        // DO THIS RECURSIVELY UNLESS IT'S LAST PAGE
+                        if(!this.props.isLast) {
+                            loadUntilScrollable();
+                        }
+                    }
+                );
+            }
+        };
+
+
         this.props.memoListRequest(true).then(
             () => {
+                loadUntilScrollable();
                 loadMemoLoop();
             }
         );
+
+        $(window).scroll(() => {
+            // WHEN HEIGHT UNDER SCROLLBOTTOM IS LESS THEN 250
+            if ($(document).height() - $(window).height() - $(window).scrollTop() < 250) {
+                if(!this.state.loadingState) {
+                    this.loadOldMemo();
+                    this.setState({
+                        loadingState: true
+                    });
+                }
+            } else {
+                if(this.state.loadingState) {
+                    this.setState({
+                        loadingState: false
+                    });
+                }
+            }
+        });
     }
 
     componentWillUnmount() {
+        // STOPS THE loadMemoLoop
         clearTimeout(this.memoLoaderTimeoutId);
+
+        // REMOVE WINDOWS SCROLL LISTENER
+        $(window).unbind();
     }
 
     loadNewMemo() {
@@ -39,8 +81,6 @@ class Home extends React.Component {
             });
         }
 
-
-
         // IF PAGE IS EMPTY, DO THE INITIAL LOADING
         if(this.props.memoData.length === 0 )
             return this.props.memoListRequest(true);
@@ -48,6 +88,28 @@ class Home extends React.Component {
 
 
         return this.props.memoListRequest(false, 'new', this.props.memoData[0]._id);
+    }
+
+    loadOldMemo() {
+        // CANCEL IF USER IS READING THE LAST PAGE
+        if(this.props.isLast) {
+            return new Promise(
+                (resolve, reject)=> {
+                    resolve();
+                }
+            );
+        }
+
+        // GET ID OF THE MEMO AT THE BOTTOM
+        let lastId = this.props.memoData[this.props.memoData.length - 1]._id;
+
+        // START REQUEST
+        return this.props.memoListRequest(false, 'old', lastId).then(() => {
+            // IF IT IS LAST PAGE, NOTIFY
+            if(this.props.isLast) {
+                Materialize.toast('You are reading the last page', 2000);
+            }
+        });
     }
 
     handlePost(contents) {
@@ -91,7 +153,6 @@ class Home extends React.Component {
     }
 
     render() {
-
         const write = (<Write onPost={this.handlePost}/>);
 
         return (
@@ -109,7 +170,8 @@ const mapStateToProps = (state) => {
         postStatus: state.memo.post,
         currentUser: state.authentication.status.currentUser,
         memoData: state.memo.list.data,
-        listStatus: state.memo.list.status
+        listStatus: state.memo.list.status,
+        isLast: state.memo.list.isLast
     };
 };
 
