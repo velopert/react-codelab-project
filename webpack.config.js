38e7574417f96b9b3b33c495024dcbc89965919a
velopert <public.velopert@gmail.com>
2016-08-04 01:30:02 +0900

    00. INITIAL PROJECT

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.js',
 
@@ -6,16 +8,28 @@ module.exports = {
         filename: 'bundle.js'
     },
 
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/',
+    },
+
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['babel?' + JSON.stringify({
+                loaders: ['react-hot', 'babel?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 })],
                 exclude: /node_modules/,
             }
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
