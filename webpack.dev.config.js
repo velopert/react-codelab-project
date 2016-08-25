d88d7139d61a9f7acabe18a94d780fcceb09513f
velopert <public.velopert@gmail.com>
2016-08-25 16:36:19 +0900

    proxy **

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -22,7 +22,7 @@ module.exports = {
         historyApiFallback: true,
         contentBase: './public',
         proxy: {
-            "*": "http://localhost:3000"
+            "**": "http://localhost:3000"
         },
         stats: {
           // Config for minimal console.log mess.
